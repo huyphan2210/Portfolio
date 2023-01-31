ccefc11e6c12f53f8499fbf159ee5538b06ffbb7
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-01-31 14:42:13 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'OrbitControls'
 
 // Scene == container
 const scene = new THREE.Scene();
